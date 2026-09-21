2b263ee2b00c8e6eafdac30d23ea2432f62dcfe9
Sj14122004 <joshishivam2004@gmail.com>
2026-09-21 06:16:08 +0530

    fix cross origin auth cookie

--- apps/Backend/src/controllers/authController.ts
+++ apps/Backend/src/controllers/authController.ts
@@ -22,7 +22,7 @@ const login = async (req: Request, res: Response) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none"
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
   });
 
